f1c6a6598b61ad01cec237faea4464f30f3d908e
Denis <dbalagov@holbi.co.uk>
2018-09-20 10:42:48 +0300

    add progress bar

--- js/plugin.js
+++ js/plugin.js
@@ -7,12 +7,14 @@ class VideoPlayer {
         this.toggle = this.player.querySelector('.toggle');
         this.skipButtons = this.player.querySelectorAll('[data-skip]');
         this.ranges = this.player.querySelectorAll('.player_slider');
+        this.progresMouseDown = false;
     }
     init() {
         this.events();
     }
     events() {
         this.video.addEventListener('click', e => this.handleTogglePlay());
+        this.video.addEventListener('timeupdate', e => this.handleProgress(e));
         this.toggle.addEventListener('click', e => this.handleTogglePlay());
         this.ranges.forEach( range => {
             range.addEventListener('change', e => this.handleRangeUpdate(e));
@@ -21,6 +23,10 @@ class VideoPlayer {
         this.skipButtons.forEach( button => {
             button.addEventListener('click', e => this.handleSkip(e));
         });
+        this.progress.addEventListener('click', e => this.scrubHandle(e));
+        this.progress.addEventListener('mousemove', e => this.progresMouseDown && this.scrubHandle(e));
+        this.progress.addEventListener('mousedown', () => this.progresMouseDown = true);
+        this.progress.addEventListener('mouseup', () => this.progresMouseDown = false);
     }
     handleTogglePlay() {
         const method = this.video.paused ? 'play' : 'pause';
@@ -33,6 +39,13 @@ class VideoPlayer {
     }
     handleSkip(event) {
         this.video.currentTime += parseInt(event.target.dataset.skip);
+    }
+    handleProgress(event) {
+        const percent = (this.video.currentTime / this.video.duration).toFixed(2) * 100;
+        this.progressBar.style.flexBasis = `${percent}%`;
+    }
+    scrubHandle(event) {
+        this.video.currentTime =  (event.offsetX / this.progress.offsetWidth) * this.video.duration;
     }
 }
 const myPlayer = new VideoPlayer();
